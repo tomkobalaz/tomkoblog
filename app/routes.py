2875ef58e2ed2c33da0bd6fd6267dadc60ee4db0
unknown <Administrator@XCLBR.STAGING>
2018-07-19 15:22:53 +0200

    user added

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Tomko'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Tomkoblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
